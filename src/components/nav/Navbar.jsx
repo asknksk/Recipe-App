e932624befcf9f2f511040d10fb531ea42850e7f
Askin <asknkskk@gmail.com>
2022-07-13 23:07:01 +0300

    navBar

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,7 +1,32 @@
-import NavbarStyle from "./Navbar.style";
+import { Link } from "react-router-dom";
+import Nav, { Logo, Menu, MenuLink, Hamburger } from "./Navbar.style";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Nav justify="space-between" wrap="wrap">
+      <Logo to="/">
+        <i>{"<asknksk>"}</i>
+        <span>Recipe</span>
+      </Logo>
+
+      <Hamburger onClick={setIsOpen(!isOpen)}>
+        <GiHamburgerMenu />
+      </Hamburger>
+
+      <Menu isOpen={isOpen} onclick={() => setIsOpen(false)}>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="about">About</MenuLink>
+        <MenuLink to="register">Register</MenuLink>
+        <MenuLink to="login" onClick={() => sessionStorage.clear()}>
+          Logout
+        </MenuLink>
+      </Menu>
+    </Nav>
+  );
 };
 
 export default Navbar;
