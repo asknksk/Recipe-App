1e1ea3ab2d69a7241c3b01554364ef4f352aa056
Askin <asknkskk@gmail.com>
2022-07-14 21:22:35 +0300

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,42 @@
-import HeaderStyle from "./Header.style";
+import {
+  Button,
+  FoodInput,
+  FormContainer,
+  HeaderContainer,
+  MainHeader,
+  Select,
+} from "./Header.style";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ setQuery, setSelectedMeal, mealTypes, getData }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getData();
+  };
+
+  return (
+    <HeaderContainer>
+      <MainHeader>Recipe App</MainHeader>
+      <FormContainer onSubmit={handleSubmit}>
+        <FoodInput
+          type="text"
+          placeholder="search"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <Button>SEARCH</Button>
+        <Select
+          name="mealType"
+          id="mealType"
+          onChange={(e) => setSelectedMeal(e.target.value)}
+        >
+          {mealTypes.map((meal, index) => {
+            <option value={meal.toLowerCase()} key={index}>
+              {meal}
+            </option>;
+          })}
+        </Select>
+      </FormContainer>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,44 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import HomeStyle from "./Home.style";
+import Header from "../../components/header/Header";
+import Cards from "../../components/cards/Cards";
 
 const Home = () => {
-  return <div>Home</div>;
+  const mealTypes = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
+  const [query, setQuery] = useState("egg");
+  const [selectedMeal, setSelectedMeal] = useState(mealTypes[0]);
+  const { recipes, setRecipes } = useState("");
+
+  const APP_ID = "ba701df0";
+  const APP_KEY = "3273c964f38fee1e5af1e853b976e4df";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+
+  const getData = async () => {
+    if (query) {
+      try {
+        const { data } = await axios.get(url);
+        setRecipes(data.hits);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      alert("Please Enter your meal");
+    }
+  };
+  console.log(recipes);
+  return (
+    <div>
+      <Header
+        setQuery={setQuery}
+        setSelectedMeal={setSelectedMeal}
+        mealTypes={mealTypes}
+        getData={getData}
+      />
+      <Cards recipes={recipes} />
+    </div>
+  );
 };
 
 export default Home;
